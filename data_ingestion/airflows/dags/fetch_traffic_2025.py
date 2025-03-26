51a87a92ef3bc1c32bc157d1bcc0ac2e899df7fb
Muhammad Tashfeen Abbasi <abbasitashfeen7@gmail.com>
2025-03-26 23:57:19 +0500

    traffic2025_dag

--- data_ingestion/airflows/dags/fetch_traffic_2025.py
+++ data_ingestion/airflows/dags/fetch_traffic_2025.py
@@ -14,7 +14,6 @@ TWITTER_API_SECRET = os.getenv('TWITTER_API_SECRET')
 TWITTER_ACCESS_TOKEN = os.getenv('TWITTER_ACCESS_TOKEN')
 TWITTER_ACCESS_SECRET = os.getenv('TWITTER_ACCESS_SECRET')
 TWITTER_BEARER_TOKEN = os.getenv('TWITTER_BEARER_TOKEN')
-
 def ingest_traffic_data_2025():
     data_path = '/home/tashi/BDM_Project/2025_03_Marc_ITINERARIS_ITINERARIS.csv'  
     df = pd.read_csv(data_path)
